90f5e8ad391ad035df62df7f390e2780ab5b8fad
jcbmrt <87323881+jcbmrt@users.noreply.github.com>
2025-11-06 07:30:43 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import cloudflare from "@astrojs/cloudflare";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
-  base: "/app",
+  base: "/animals",
   build: {
-    assetsPrefix: "/app",
+    assetsPrefix: "/animals",
   },
   security: {
     checkOrigin: false,
@@ -19,13 +17,10 @@ export default defineConfig({
       enabled: true,
     },
   }),
-
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
     resolve: {
-      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
-      // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
       alias: import.meta.env.PROD
         ? {
             "react-dom/server": "react-dom/server.edge",
